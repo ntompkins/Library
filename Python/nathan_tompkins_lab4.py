b93e93b6d16f33915f37873c0832fdde979f780f
ntompkins <nathant101@gmail.com>
2015-09-23 19:54:54 +0000

    Put the code on a juice cleanse.

---
+++ Python/nathan_tompkins_lab4.py
@@ -0,0 +1,10 @@
+# nathan_tompkins_lab4.py
+# name: Nathan Tompkins
+# date: 9.23.15
+
+
+# Lab 2 base.
+# Create a while loop that will run
+# conversions until they enter an
+# invalid unit. After three invalid
+# units then the program will close.
