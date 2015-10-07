40bf959a478cb7bf7b11558e31e96d659e378ed7
ntompkins <nathant101@gmail.com>
2015-10-07 19:38:19 +0000

    Updated files

--- python/ic5.py
+++ python/ic5.py
@@ -1,60 +1,61 @@
 # nathan_tompkins_ic5.py
-# Nathan Tompkins
-# 10.5.15
+# Nathan Tompkins | 10.5.15
 
-def milesToKm():
-    quantity = float(input("Enter your miles: "))
+def results():
+    print(quantity,unit,"=",new_quantity,new_unit)
     
+# Miles to Kilometers function
+def milesToKm():
     if quantity < 0:
         print("I\'m sorry, that's not a valid input.")
     else:
         new_quantity = quantity / 1.6
-        print(new_quantity,"\n")
-    
+
+# Fahrenheit to Celsius function
 def FahToCel():
-    quantity = float(input("Enter your degrees in Fahrenheit: "))
-    
     if quantity > 1000:
         print("I\'m sorry, that's not a valid input.")
     else:
         new_quantity = (quantity - 32)*(5/9)
-        print(new_quantity,"\n")
-        
+
+# Gallons to Liters function
 def GalToLit():
-    quantity = float(input("Enter your gallons: "))
-    
     if quantity < 0:
         print("I\'m sorry, that's not a valid input.")
-    else: 
+    else:
         new_quantity = quantity * 3.785
         print(new_quantity,"\n")
-    
+
+# Pounds to Kilograms function
 def PoundsToKg():
-    quantity = float(input("Enter your pounds: "))
-    
     if quantity < 0:
         print("I\'m sorry, that's not a valid input.")
     else:
         new_quantity = quantity * .45
         print(new_quantity,"\n")
 
+# Inches to Centimeters function
 def InchesToCm():
-    quantity = float(input("Enter your inches: "))
-    
     if quantity < 0:
         print("I\'m sorry, that's not a valid input.")
     else:
         new_quantity = quantity * 2.54
         print(new_quantity,"\n")
 
+# main function
 def main():
     print("Unit Converter")
-    quantity = float(input("What is your quantity:")
-    milesToKm(quantity)
+    global quantity
+    quantity = float(input("Your quantity: "))
+    
+    milesToKm()
+    unit = "Miles"
+    new_unit = "Kilometers"
+    results()
+    
     FahToCel()
     GalToLit()
     PoundsToKg()
     InchesToCm()
     print("End")
-    
 main()
