c7a07c9cec63236372d8df4ab100957edb410ab2
ntompkins <nathant101@gmail.com>
2015-10-07 18:53:48 -0500

    Updated contents

--- python/ic5.py
+++ python/ic5.py
@@ -1,60 +1,54 @@
 # nathan_tompkins_ic5.py
-# Nathan Tompkins
-# 10.5.15
+# Nathan Tompkins | 10.5.15
 
+# Miles to Kilometers function
 def milesToKm():
-    quantity = float(input("Enter your miles: "))
-    
     if quantity < 0:
         print("I\'m sorry, that's not a valid input.")
     else:
         new_quantity = quantity / 1.6
         print(new_quantity,"\n")
-    
+
+# Fahrenheit to Celsius function
 def FahToCel():
-    quantity = float(input("Enter your degrees in Fahrenheit: "))
-    
     if quantity > 1000:
         print("I\'m sorry, that's not a valid input.")
     else:
         new_quantity = (quantity - 32)*(5/9)
         print(new_quantity,"\n")
-        
+
+# Gallons to Liters function
 def GalToLit():
-    quantity = float(input("Enter your gallons: "))
-    
     if quantity < 0:
         print("I\'m sorry, that's not a valid input.")
-    else: 
+    else:
         new_quantity = quantity * 3.785
         print(new_quantity,"\n")
-    
+
+# Pounds to Kilograms function
 def PoundsToKg():
-    quantity = float(input("Enter your pounds: "))
-    
     if quantity < 0:
         print("I\'m sorry, that's not a valid input.")
     else:
         new_quantity = quantity * .45
         print(new_quantity,"\n")
 
+# Inches to Centimeters function
 def InchesToCm():
-    quantity = float(input("Enter your inches: "))
-    
     if quantity < 0:
         print("I\'m sorry, that's not a valid input.")
     else:
         new_quantity = quantity * 2.54
         print(new_quantity,"\n")
 
+# main function
 def main():
     print("Unit Converter")
-    quantity = float(input("What is your quantity:")
+    quantity = float(input("Your quantity: "))
     milesToKm(quantity)
-    FahToCel()
-    GalToLit()
-    PoundsToKg()
-    InchesToCm()
+    FahToCel(quantity)
+    GalToLit(quantity)
+    PoundsToKg(quantity)
+    InchesToCm(quantity)
     print("End")
-    
 main()
